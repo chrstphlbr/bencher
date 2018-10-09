f72716f18ca07f4dc3d4f9deb252d9b2d7a470ba
Christoph Laaber <christoph.laaber@gmail.com>
2018-10-09 18:22:52 +0200

    added stream transformation of JMH results (in JMHResultTransformer)

--- src/main/kotlin/ch/uzh/ifi/seal/bencher/jmh_results/JMHResultParser.kt
+++ src/main/kotlin/ch/uzh/ifi/seal/bencher/jmh_results/JMHResultParser.kt
@@ -1,139 +1,37 @@
 package ch.uzh.ifi.seal.bencher.jmh_results
 
 import com.beust.klaxon.JsonArray
-import com.beust.klaxon.JsonObject
+import com.beust.klaxon.KlaxonException
 import com.beust.klaxon.Parser
 import org.funktionale.either.Either
 import java.io.File
 import java.io.FileInputStream
 
 class JMHResultParser(
-        private val inFile: File,
+        inFile: File,
         private val project: String,
         private val commit: String,
-        private val trial: Int) {
+        private val trial: Int
+) : Iterable<BenchmarkResult> {
 
-    private val jmhVersion = "jmhVersion"
-    private val benchmark = "benchmark"
-    private val params = "params"
-    private val mode = "mode"
-    private val threads = "threads"
-    private val forks = "forks"
-    private val warmupIterations = "warmupIterations"
-    private val warmupTime = "warmupTime"
-    private val measurementIterations = "measurementIterations"
-    private val measurementTime = "measurementTime"
-    private val primaryMetric = "primaryMetric"
-    private val unit = "scoreUnit"
-    private val values = "rawData"
-    private val valuesHist = "rawDataHistogram"
-
-    fun parse(): Either<String, JMHResult> {
-        val json = Parser().parse(FileInputStream(inFile))
-
-        if (json is JsonArray<*>) {
-            return Either.right(parse(json))
+    private val json: JsonArray<*>
+    init {
+        val j = Parser().parse(FileInputStream(inFile)) ?: throw KlaxonException("Could not parse file '${inFile.absolutePath}'")
+        if (j is JsonArray<*>) {
+            json = j
+        } else {
+            throw KlaxonException("Invalid json: no array at root")
         }
-
-        return Either.left("Invalid json: no array at root")
     }
 
-    private fun parse(arr: JsonArray<*>): JMHResult =
+    override fun iterator(): Iterator<BenchmarkResult> = JMHResultIterator(json)
+
+    fun parseAll(): Either<String, JMHResult> = Either.right(
             JMHResult(
                     project = project,
                     commit = commit,
                     trial = trial,
-                    benchmarks = arr.mapNotNull { bench ->
-                        if (bench is JsonObject) {
-                            parseBench(bench)
-                        } else {
-                            null
-                        }
-                    }
+                    benchmarks = iterator().asSequence().toList()
             )
-
-    private fun parseBench(obj: JsonObject): BenchmarkResult? {
-        val v = obj.string(jmhVersion) ?: return null
-        val n = obj.string(benchmark) ?: return null
-        val m = obj.string(mode) ?: return null
-        val t = obj.int(threads) ?: return null
-        val f = obj.int(forks) ?: return null
-        val wi = obj.int(warmupIterations) ?: return null
-        val wt = obj.string(warmupTime) ?: return null
-        val mi = obj.int(measurementIterations) ?: return null
-        val mt = obj.string(measurementTime) ?: return null
-
-        // jmh params
-        val ps: List<Pair<String, String>> = if (obj.containsKey(params)) {
-            val po =obj.obj(params)
-            // should always be non-null
-            po!!.map { Pair(it.key, "${it.value}") }
-        } else {
-            listOf()
-        }
-
-        val pm = obj.obj(primaryMetric) ?: return null
-
-        val u = pm.string(unit) ?: return null
-
-        val vals = if (pm.containsKey(values)) {
-            val arr = pm.array<JsonArray<Float>>(values) ?: return null
-            parseValues(arr)
-        } else if (pm.containsKey(valuesHist)) {
-            val arr = pm.array<JsonArray<JsonArray<JsonArray<Float>>>>(valuesHist) ?: return null
-            parseHistValues(arr)
-        } else {
-            null
-        } ?: return null
-
-
-        return BenchmarkResult(
-                jmhVersion = v,
-                name = n,
-                jmhParams = ps,
-                mode = m,
-                threads = t,
-                forks = f,
-                warmupIterations = wi,
-                warmuptTime = wt,
-                measurementIterations = mi,
-                measurementTime = mt,
-                unit = u,
-                values = vals
-        )
-    }
-
-    private fun parseValues(arr: JsonArray<JsonArray<Float>>): List<ForkResult> {
-        val frs = arr.mapIndexed { fork, iters ->
-            ForkResult(
-                    fork = fork+1,
-                    iterations = iters.mapIndexed { iter, value ->
-                        IterationResult(
-                                iteration = iter+1,
-                                invocations = listOf(value)
-                        )
-                    }
-            )
-        }.toList()
-        return frs
-    }
-
-    private fun parseHistValues(arr: JsonArray<JsonArray<JsonArray<JsonArray<Float>>>>): List<ForkResult> {
-        val frs = arr.mapIndexed { fork, iters ->
-            ForkResult(
-                    fork = fork+1,
-                    iterations = iters.mapIndexed { iter, values ->
-                        IterationResult(
-                                iteration = iter+1,
-                                invocations = values.flatMap { value ->
-                                    val v = value[0]
-                                    val c = value[1]
-                                    List(c.toInt()) { v }
-                                }
-                        )
-                    }
-            )
-        }.toList()
-        return frs
-    }
+    )
 }
--- src/main/kotlin/ch/uzh/ifi/seal/bencher/jmh_results/JMHResultPrinter.kt
+++ src/main/kotlin/ch/uzh/ifi/seal/bencher/jmh_results/JMHResultPrinter.kt
@@ -6,7 +6,17 @@ import java.io.OutputStream
 import java.io.OutputStreamWriter
 
 interface JMHResultPrinter {
-    fun print(res: JMHResult)
+    fun printHeader()
+    fun done()
+    fun print(project: String, commit: String, trial: Int, benchmarkResult: BenchmarkResult)
+
+    fun printAll(res: JMHResult) {
+        printHeader()
+        res.benchmarks.forEach {
+            print(res.project, res.commit, res.trial, it)
+        }
+        done()
+    }
 }
 
 class JSONResultPrinter(
@@ -14,33 +24,28 @@ class JSONResultPrinter(
         charset: String = Constants.defaultCharset,
         private val flushPoint: FlushPoint = FlushPoint.Benchmark
 ) : JMHResultPrinter {
-
     private val csvHeader = "project;commit;benchmark;params;trial;fork;iteration;mode;unit;value"
     private val csvLine = "%s;%s;%s;%s;%d;%d;%d;%s;%s;%f"
 
     private val w: BufferedWriter = BufferedWriter(OutputStreamWriter(os, charset))
 
-    override fun print(res: JMHResult) {
-        printHeader()
-        res.benchmarks.forEach { br ->
-            br.values.forEach { forkResult ->
-                forkResult.iterations.forEach { iterResult ->
-                    iterResult.invocations.forEach {
-                        w.write(csvLine.format(
-                                res.project,
-                                res.commit,
-                                br.name,
-                                br.jmhParams.fold("") { acc, e -> "$acc${e.first}=${e.second}," }.substringBeforeLast(","),
-                                res.trial,
-                                forkResult.fork,
-                                iterResult.iteration,
-                                br.mode,
-                                br.unit,
-                                it
-                        ))
-                        w.write("\n")
-                        flush()
-                    }
+    override fun print(project: String, commit: String, trial: Int, benchmarkResult: BenchmarkResult) {
+        benchmarkResult.values.forEach { forkResult ->
+            forkResult.iterations.forEach { iterResult ->
+                iterResult.invocations.forEach {
+                    w.write(csvLine.format(
+                            project,
+                            commit,
+                            benchmarkResult.name,
+                            benchmarkResult.jmhParams.fold("") { acc, e -> "$acc${e.first}=${e.second}," }.substringBeforeLast(","),
+                            trial,
+                            forkResult.fork,
+                            iterResult.iteration,
+                            benchmarkResult.mode,
+                            benchmarkResult.unit,
+                            it
+                    ))
+                    w.write("\n")
                     flush()
                 }
                 flush()
@@ -48,14 +53,19 @@ class JSONResultPrinter(
             flush()
         }
         flush()
-        w.close()
     }
 
-    private fun printHeader() {
+    override fun printHeader() {
         w.write(csvHeader)
         w.write("\n")
         w.flush()
     }
+
+    override fun done() {
+        flush()
+        w.close()
+    }
+
 
     private fun flush() {
         when (flushPoint) {
