200bd4e93b8ddfa0241731ff86b4ec1ee986fcbe
Christoph Laaber <christoph.laaber@gmail.com>
2019-01-22 18:08:54 +0100

    changed toString for IncludeAll and IncludeOnly

--- src/main/kotlin/ch/uzh/ifi/seal/bencher/analysis/callgraph/sta/Inclusions.kt
+++ src/main/kotlin/ch/uzh/ifi/seal/bencher/analysis/callgraph/sta/Inclusions.kt
@@ -1,7 +1,11 @@
 package ch.uzh.ifi.seal.bencher.analysis.callgraph.sta
 
 sealed class WalaSCGInclusions
-object IncludeAll : WalaSCGInclusions()
+object IncludeAll : WalaSCGInclusions() {
+    override fun toString(): String = "IncludeAll"
+}
 data class IncludeOnly(
         val includes: Set<String>
-) : WalaSCGInclusions()
+) : WalaSCGInclusions() {
+    override fun toString(): String = includes.joinToString(separator = ",", prefix = "IncludeOnly[", postfix = "]")
+}
