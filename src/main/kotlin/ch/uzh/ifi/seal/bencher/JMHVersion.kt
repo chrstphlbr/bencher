5ac10b91926387dd596041c8da3e81f5b31871ae
stewue <stefan@wuersten.ch>
2019-08-21 16:07:03 +0200

    Add property patch to JmhVersion data class (and update test cases)

--- src/main/kotlin/ch/uzh/ifi/seal/bencher/JMHVersion.kt
+++ src/main/kotlin/ch/uzh/ifi/seal/bencher/JMHVersion.kt
@@ -2,7 +2,8 @@ package ch.uzh.ifi.seal.bencher
 
 data class JMHVersion(
         val major: Int,
-        val minor: Int
+        val minor: Int,
+        val patch: Int = 0
 ) : Comparable<JMHVersion> {
 
     override fun compareTo(other: JMHVersion): Int =
@@ -11,6 +12,8 @@ data class JMHVersion(
             } else if (this.major < other.major) {
                 -1
             } else if (this.major == other.major && this.minor < other.minor) {
+                -1
+            } else if (this.major == other.major && this.minor == other.minor && this.patch < other.patch) {
                 -1
             } else {
                 1
