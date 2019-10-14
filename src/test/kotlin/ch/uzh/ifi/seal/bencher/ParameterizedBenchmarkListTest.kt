b7ecbf44192ae1f827eff753786035294aacbfda
Christoph Laaber <christoph.laaber@gmail.com>
2019-10-14 13:45:08 +0200

    added prioritization of parameterized benchmarks where parameters are in reversed order

--- src/test/kotlin/ch/uzh/ifi/seal/bencher/ParameterizedBenchmarkListTest.kt
+++ src/test/kotlin/ch/uzh/ifi/seal/bencher/ParameterizedBenchmarkListTest.kt
@@ -13,12 +13,29 @@ class ParameterizedBenchmarkListTest {
     }
 
     @Test
+    fun emptyRev() {
+        val bs = listOf<Benchmark>()
+        val pbs = bs.parameterizedBenchmarks(reversed = true)
+        Assertions.assertEquals(0, pbs.size)
+    }
+
+    @Test
     fun nonParamBenchs() {
         val bs = listOf(
                 JarTestHelper.BenchNonParameterized.bench2,
                 JarTestHelper.OtherBench.bench3
         )
         val pbs = bs.parameterizedBenchmarks()
+        Assertions.assertEquals(bs, pbs)
+    }
+
+    @Test
+    fun nonParamBenchsRev() {
+        val bs = listOf(
+                JarTestHelper.BenchNonParameterized.bench2,
+                JarTestHelper.OtherBench.bench3
+        )
+        val pbs = bs.parameterizedBenchmarks(reversed = true)
         Assertions.assertEquals(bs, pbs)
     }
 
@@ -34,10 +51,29 @@ class ParameterizedBenchmarkListTest {
     }
 
     @Test
+    fun oneParamBenchRev() {
+        val bs = listOf(
+                JarTestHelper.BenchNonParameterized.bench2,
+                JarTestHelper.BenchParameterized2.bench4
+        )
+        val pbs = bs.parameterizedBenchmarks(reversed = true)
+        val ebs = bs.flatMap { it.parameterizedBenchmarks(reversed = true) }
+        Assertions.assertEquals(ebs, pbs)
+    }
+
+    @Test
     fun idempotence() {
         val b = JarTestHelper.BenchParameterized2.bench4.parameterizedBenchmarks()[0]
         val bs = listOf(b)
         val pbs = bs.parameterizedBenchmarks()
+        Assertions.assertEquals(bs, pbs)
+    }
+
+    @Test
+    fun idempotenceRev() {
+        val b = JarTestHelper.BenchParameterized2.bench4.parameterizedBenchmarks()[0]
+        val bs = listOf(b)
+        val pbs = bs.parameterizedBenchmarks(reversed = true)
         Assertions.assertEquals(bs, pbs)
     }
 }
