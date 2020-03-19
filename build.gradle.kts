518a8c33f8855b70cc4f09d589495a7d0bc08444
Christoph Laaber <christoph.laaber@gmail.com>
2020-03-19 18:13:39 +0100

    switched to Kotlin Gradle build files

---
+++ build.gradle.kts
@@ -0,0 +1,129 @@
+object V {
+    const val wala = "1.5.0"
+    const val aspectj = "1.9.1"
+    const val klaxon = "3.0.11"
+    const val junit = "5.6.0"
+    const val log4j2 = "2.11.1"
+    const val funktionale = "1.2"
+    const val commonsCSV = "1.6"
+    const val commonsIO = "2.6"
+    const val asm = "7.0"
+    const val picocli = "3.9.2"
+//    const val kotlinxCoroutines = "1.1.1"
+    const val jgrapht = "1.3.0"
+    const val jmh = "1.21"
+    const val shadow = "5.0.0"
+    const val jmhGradle = "0.5.0"
+    const val eclipseJDT = "3.18.0"
+}
+
+plugins {
+    kotlin("jvm") version "1.3.70"
+    application
+    id("com.github.johnrengelman.shadow") version "5.0.0"
+    id("me.champeau.gradle.jmh") version "0.5.0"
+}
+
+group = "ch.uzh.ifi.seal"
+version = "1.0-SNAPSHOT"
+
+application {
+//    applicationDefaultJvmArgs = listOf("-Xms6G", "-Xmx8G")
+    mainClassName = "ch.uzh.ifi.seal.bencher.MainKt"
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+//    implementation(group: "org.jetbrains.kotlinx", name: "kotlinx-coroutines-core", version: V.kotlinxCoroutines)
+    implementation(group = "info.picocli", name = "picocli", version = V.picocli)
+    implementation(group = "com.beust", name = "klaxon", version = V.klaxon)
+    implementation(group = "org.funktionale", name = "funktionale-all", version = V.funktionale)
+    implementation(group = "com.ibm.wala", name = "com.ibm.wala.core", version = V.wala)
+    implementation(group = "org.apache.logging.log4j", name = "log4j-api", version = V.log4j2)
+    implementation(group = "org.apache.logging.log4j", name = "log4j-core", version = V.log4j2)
+    implementation(group = "org.ow2.asm", name = "asm", version = V.asm)
+    implementation(group = "org.apache.commons", name = "commons-csv", version = V.commonsCSV)
+    implementation(group = "commons-io", name = "commons-io", version = V.commonsIO)
+    implementation(group = "org.jgrapht", name = "jgrapht-core", version = V.jgrapht)
+    implementation(group = "org.openjdk.jmh", name = "jmh-core", version = V.jmh)
+    implementation(group = "org.eclipse.jdt", name = "org.eclipse.jdt.core", version = V.eclipseJDT)
+    jmh(group = "org.openjdk.jmh", name = "jmh-generator-annprocess", version = V.jmh)
+
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = V.junit)
+    testImplementation(group = "org.junit.jupiter", name =  "junit-jupiter-params", version = V.junit)
+    testImplementation(kotlin("test"))
+    testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = V.junit)
+}
+
+fun splitWith(c: Char, clargs: String): List<String> {
+    var inC = false
+    var ret = mutableListOf<String>()
+    var curr = ""
+
+    clargs.forEach { i ->
+        if (i == c) {
+            inC = !inC
+        } else if (i == ' ' && !inC) {
+            ret.add(curr)
+            curr = ""
+        } else {
+            curr += i
+        }
+    }
+
+    if (curr != "") {
+        ret.add(curr)
+    }
+
+    return ret
+}
+
+tasks {
+    defaultTasks("run")
+
+    named<JavaExec>("run") {
+        val clargs = System.getProperty("args")
+        if (clargs != null) {
+            args(when {
+                clargs.contains("\"") -> splitWith('"', clargs)
+                clargs.contains("\'") -> splitWith('\'', clargs)
+                else -> clargs.split(" ")
+            })
+        }
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+
+    jmhJar {
+        duplicatesStrategy = DuplicatesStrategy.WARN
+        // set jmh jar name
+        //[archiveBaseName]-[archiveAppendix]-[archiveVersion]-[archiveClassifier].[archiveExtension]
+        //TODO add classifier to JAR
+//        archiveClassifier = "jmh"
+    }
+
+    jmh {
+        jmhVersion = V.jmh
+        duplicateClassesStrategy = DuplicatesStrategy.EXCLUDE
+    }
+}
