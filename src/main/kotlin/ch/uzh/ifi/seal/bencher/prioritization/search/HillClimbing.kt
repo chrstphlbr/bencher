2ae5201a6b6cd6e5373753f7938b31855d473c51
Christoph Laaber <christoph.laaber@gmail.com>
2023-10-13 14:57:59 +0200

    added steepest-ascent HillClimbing algorithm + tests

---
+++ src/main/kotlin/ch/uzh/ifi/seal/bencher/prioritization/search/HillClimbing.kt
@@ -0,0 +1,62 @@
+package ch.uzh.ifi.seal.bencher.prioritization.search
+
+import org.uma.jmetal.algorithm.impl.AbstractLocalSearch
+import org.uma.jmetal.problem.Problem
+import org.uma.jmetal.solution.Solution
+import org.uma.jmetal.util.neighborhood.Neighborhood
+
+// HillClimbing is an implementation of the steepest ascent version hill climbing algorithm
+class HillClimbing<S>(
+    private val initialSolution: S, // expects an evaluated solution
+    private val problem: Problem<S>,
+    private val comparator: Comparator<S>,
+    private val neighborhood: Neighborhood<S>,
+) : AbstractLocalSearch<S>() where S : Solution<*> {
+
+    private var currentChanged: Boolean = true
+    private val evaluatedSolutions: MutableMap<S, S> = mutableMapOf()
+    private var evaluations: Int = 0
+
+    override fun name(): String = "Hill Climbing"
+
+    override fun description(): String = "Hill Climbing"
+
+    override fun setCurrentSolution(): S = initialSolution
+
+    override fun initProgress() {
+        evaluations = 1
+    }
+
+    override fun updateProgress() {
+        evaluations++
+    }
+
+    override fun isStoppingConditionReached(): Boolean {
+        return !currentChanged
+    }
+
+    override fun updateCurrentSolution(currentSolution: S): S {
+        // this assumes an implementation like PermutationNeighborhood, where the neighborhood is not calculated
+        // from the solutionList but on the spot for a single solution from the solutionList
+        val ns = neighborhood.getNeighbors(listOf(currentSolution), 0)
+        if (ns.isEmpty()) {
+            currentChanged = false
+            return currentSolution
+        }
+
+        val betterNeighbors = ns.asSequence()
+            .map { memorizedEvaluate(it) }
+            .sortedWith(comparator)
+            .takeWhile { comparator.compare(it, currentSolution) < 0 }
+            .toList()
+
+        if (betterNeighbors.isEmpty()) {
+            currentChanged = false
+            return currentSolution
+        }
+
+        return betterNeighbors[0]
+    }
+
+    private fun memorizedEvaluate(solution: S): S = evaluatedSolutions.getOrPut(solution) { problem.evaluate(solution) }
+}
---
+++ src/test/kotlin/ch/uzh/ifi/seal/bencher/prioritization/search/HillClimbingTest.kt
@@ -0,0 +1,83 @@
+package ch.uzh.ifi.seal.bencher.prioritization.search
+
+import ch.uzh.ifi.seal.bencher.Benchmark
+import ch.uzh.ifi.seal.bencher.analysis.JarTestHelper
+import ch.uzh.ifi.seal.bencher.prioritization.PrioritizedMethod
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.uma.jmetal.problem.permutationproblem.PermutationProblem
+import org.uma.jmetal.solution.permutationsolution.PermutationSolution
+
+class HillClimbingTest : AlgorithmTest() {
+
+    private fun comparator(objectives: List<Objective>, normalize: Boolean): Comparator<PermutationSolution<Int>> =
+        AggregateObjectiveComparator(aggregate(objectives, normalize))
+
+    private fun prepareInitialSolution(
+        benchs: List<Benchmark>,
+        p: PermutationProblem<PermutationSolution<Int>>,
+    ): PermutationSolution<Int> {
+        val fixedInitialSolution = p.createSolution()
+        benchs.forEachIndexed { i, b ->
+            val id = benchmarkIdMap[b]!!
+            fixedInitialSolution.variables()[i] = id
+        }
+
+        return p.evaluate(fixedInitialSolution)
+    }
+
+    private fun runHillClimbing(benchs: List<Benchmark>): List<PrioritizedMethod<Benchmark>> {
+        val objectives = listOf(covObjective, covOverlapObjective, changeHistoryObjective)
+
+        val p = PrioritizationProblem(benchmarkIdMap, objectives)
+        val comp = comparator(objectives, false)
+
+        val initial = prepareInitialSolution(benchs, p)
+
+        val a = HillClimbing(initial, p, comp, PermutationNeighborhood())
+
+        return runAlgorithmCheckResult(a)
+    }
+
+    @Test
+    fun benchOrder() {
+        val result = runHillClimbing(benchs)
+
+        Assertions.assertEquals(JarTestHelper.BenchParameterized.bench1, result[0].method)
+        Assertions.assertEquals(JarTestHelper.BenchNonParameterized.bench2, result[1].method)
+        Assertions.assertEquals(JarTestHelper.OtherBench.bench3, result[2].method)
+        Assertions.assertEquals(JarTestHelper.BenchParameterized2.bench4, result[3].method)
+    }
+
+    @Test
+    fun reverseBenchOrder() {
+        val result = runHillClimbing(benchs.reversed())
+
+        Assertions.assertEquals(JarTestHelper.BenchParameterized.bench1, result[0].method)
+        Assertions.assertEquals(JarTestHelper.OtherBench.bench3, result[1].method)
+        Assertions.assertEquals(JarTestHelper.BenchNonParameterized.bench2, result[2].method)
+        Assertions.assertEquals(JarTestHelper.BenchParameterized2.bench4, result[3].method)
+    }
+
+    @Test
+    fun orderBenchmarks2314() {
+        val benchOrder = listOf(benchs[1], benchs[2], benchs[0], benchs[3])
+        val result = runHillClimbing(benchOrder)
+
+        Assertions.assertEquals(JarTestHelper.BenchParameterized.bench1, result[0].method)
+        Assertions.assertEquals(JarTestHelper.OtherBench.bench3, result[1].method)
+        Assertions.assertEquals(JarTestHelper.BenchParameterized2.bench4, result[2].method)
+        Assertions.assertEquals(JarTestHelper.BenchNonParameterized.bench2, result[3].method)
+    }
+
+    @Test
+    fun orderBenchmarks3241() {
+        val benchOrder = listOf(benchs[2], benchs[1], benchs[3], benchs[0])
+        val result = runHillClimbing(benchOrder)
+
+        Assertions.assertEquals(JarTestHelper.BenchParameterized.bench1, result[0].method)
+        Assertions.assertEquals(JarTestHelper.BenchNonParameterized.bench2, result[1].method)
+        Assertions.assertEquals(JarTestHelper.BenchParameterized2.bench4, result[2].method)
+        Assertions.assertEquals(JarTestHelper.OtherBench.bench3, result[3].method)
+    }
+}
