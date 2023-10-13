23a8ac7c5e6d00e96f12681888b1ffb4c8c9eb16
Christoph Laaber <christoph.laaber@gmail.com>
2023-10-13 11:08:59 +0200

    extracted common test functionality in GreedyTest (with other algorithm tests) into AlgorithmTest

---
+++ src/test/kotlin/ch/uzh/ifi/seal/bencher/prioritization/search/AlgorithmTest.kt
@@ -0,0 +1,67 @@
+package ch.uzh.ifi.seal.bencher.prioritization.search
+
+import arrow.core.getOrElse
+import ch.uzh.ifi.seal.bencher.Benchmark
+import ch.uzh.ifi.seal.bencher.analysis.coverage.Coverages
+import ch.uzh.ifi.seal.bencher.analysis.weight.CoverageUnitWeights
+import ch.uzh.ifi.seal.bencher.analysis.weight.CoveragesWeighter
+import ch.uzh.ifi.seal.bencher.measurement.PerformanceChangesTestHelper
+import ch.uzh.ifi.seal.bencher.prioritization.PrioritizedMethod
+import ch.uzh.ifi.seal.bencher.prioritization.PrioritizerTestHelper
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.uma.jmetal.algorithm.Algorithm
+import org.uma.jmetal.solution.permutationsolution.PermutationSolution
+import org.uma.jmetal.util.aggregationfunction.impl.WeightedSum
+
+abstract class AlgorithmTest {
+    protected lateinit var benchs: List<Benchmark>
+    protected lateinit var benchmarkIdMap: BenchmarkIdMap
+    protected lateinit var cov: Coverages
+    protected lateinit var weights: CoverageUnitWeights
+    protected lateinit var covObjective: CoverageObjective
+    protected lateinit var covOverlapObjective: CoverageOverlapObjective
+    protected lateinit var changeHistoryObjective: ChangeHistoryObjective
+
+    @BeforeEach
+    fun setUp() {
+        benchs = PrioritizerTestHelper.benchs
+        benchmarkIdMap = BenchmarkIdMapImpl(benchs)
+        cov = PrioritizerTestHelper.covFull
+        weights = CoveragesWeighter(cov).weights()
+            .getOrElse { Assertions.fail("could not get CoverageUnitWeights: $it") }
+
+        // objectives
+        covObjective = CoverageObjective(cov, weights)
+        covOverlapObjective = CoverageOverlapObjective(cov)
+        changeHistoryObjective = ChangeHistoryObjective(PerformanceChangesTestHelper.changes)
+    }
+
+    protected fun aggregate(objectives: List<Objective>, normalize: Boolean = true): Aggregation = Aggregation(
+        function = WeightedSum(normalize),
+        weights = objectives.indices.map { 1.0 / objectives.size }.toDoubleArray(),
+        objectives = if (!normalize) { null } else { objectives },
+    )
+
+    protected fun runAlgorithmCheckResult(a: Algorithm<PermutationSolution<Int>>): List<PrioritizedMethod<Benchmark>> {
+        return checkResult(runAlgorithm(a))
+    }
+
+    private fun runAlgorithm(a: Algorithm<PermutationSolution<Int>>): PermutationSolution<Int> {
+        a.run()
+        return a.result()
+    }
+
+    private fun checkResult(s: PermutationSolution<Int>): List<PrioritizedMethod<Benchmark>> {
+        val results = JMetalPrioritizer.transformSolutions(benchmarkIdMap, listOf(s))
+            .getOrElse { Assertions.fail("could not transform solutions: $it") }
+
+        Assertions.assertEquals(1, results.size)
+
+        val result = results[0]
+
+        Assertions.assertEquals(4, result.size)
+
+        return result
+    }
+}
--- src/test/kotlin/ch/uzh/ifi/seal/bencher/prioritization/search/GreedyTest.kt
+++ src/test/kotlin/ch/uzh/ifi/seal/bencher/prioritization/search/GreedyTest.kt
@@ -1,73 +1,24 @@
 package ch.uzh.ifi.seal.bencher.prioritization.search
 
-import arrow.core.getOrElse
 import ch.uzh.ifi.seal.bencher.Benchmark
 import ch.uzh.ifi.seal.bencher.analysis.JarTestHelper
-import ch.uzh.ifi.seal.bencher.analysis.coverage.Coverages
-import ch.uzh.ifi.seal.bencher.analysis.weight.CoverageUnitWeights
-import ch.uzh.ifi.seal.bencher.analysis.weight.CoveragesWeighter
-import ch.uzh.ifi.seal.bencher.measurement.PerformanceChangesTestHelper
 import ch.uzh.ifi.seal.bencher.prioritization.PrioritizedMethod
-import ch.uzh.ifi.seal.bencher.prioritization.PrioritizerTestHelper
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import org.uma.jmetal.util.aggregationfunction.impl.WeightedSum
 
-class GreedyTest {
-
-    private lateinit var benchs: List<Benchmark>
-    private lateinit var benchmarkIdMap: BenchmarkIdMap
-    private lateinit var cov: Coverages
-    private lateinit var weights: CoverageUnitWeights
-    private lateinit var covObjective: CoverageObjective
-    private lateinit var covOverlapObjective: CoverageOverlapObjective
-    private lateinit var changeHistoryObjective: ChangeHistoryObjective
-
-    @BeforeEach
-    fun setUp() {
-        benchs = PrioritizerTestHelper.benchs
-        benchmarkIdMap = BenchmarkIdMapImpl(benchs)
-        cov = PrioritizerTestHelper.covFull
-        weights = CoveragesWeighter(cov).weights()
-            .getOrElse { Assertions.fail("could not get CoverageUnitWeights: $it") }
-
-        // objectives
-        covObjective = CoverageObjective(cov, weights)
-        covOverlapObjective = CoverageOverlapObjective(cov)
-        changeHistoryObjective = ChangeHistoryObjective(PerformanceChangesTestHelper.changes)
-    }
-
-    private fun aggregate(objectives: List<Objective>, normalize: Boolean = true): Aggregation = Aggregation(
-        function = WeightedSum(normalize),
-        weights = objectives.indices.map { 1.0 / objectives.size }.toDoubleArray(),
-        objectives = if (!normalize) { null } else { objectives },
-    )
+class GreedyTest : AlgorithmTest() {
 
     private fun greedyAlgorithm(objectives: List<Objective>, aggregate: Aggregation): Greedy {
         val problem = PrioritizationProblem(benchmarkIdMap, objectives)
         return Greedy(problem, benchmarkIdMap, objectives, aggregate)
     }
 
-    private fun runGreedyCheckResults(objectives: List<Objective>, normalize: Boolean): List<PrioritizedMethod<Benchmark>> {
+    private fun runGreedyCheckResult(objectives: List<Objective>, normalize: Boolean): List<PrioritizedMethod<Benchmark>> {
         val aggregate = aggregate(objectives, normalize)
-
         val a = greedyAlgorithm(objectives, aggregate)
-        a.run()
-        val s = a.result()
-
-        val results = JMetalPrioritizer.transformSolutions(benchmarkIdMap, listOf(s))
-            .getOrElse { Assertions.fail("could not transform solutions: $it") }
-
-        Assertions.assertEquals(1, results.size)
-
-        val result = results[0]
-
-        Assertions.assertEquals(4, result.size)
-
-        return result
+        return runAlgorithmCheckResult(a)
     }
 
     @ParameterizedTest
@@ -75,7 +26,7 @@ class GreedyTest {
     private fun oneObjectiveEqualWeightsCoverage(normalize: Boolean) {
         val objectives: List<Objective> = listOf(covObjective)
 
-        val result = runGreedyCheckResults(objectives, normalize)
+        val result = runGreedyCheckResult(objectives, normalize)
 
         Assertions.assertEquals(JarTestHelper.BenchParameterized.bench1, result[0].method)
         Assertions.assertEquals(JarTestHelper.BenchParameterized2.bench4, result[1].method)
@@ -88,7 +39,7 @@ class GreedyTest {
     private fun oneObjectiveEqualWeightsCoverageOverlap(normalize: Boolean) {
         val objectives: List<Objective> = listOf(covOverlapObjective)
 
-        val result = runGreedyCheckResults(objectives, normalize)
+        val result = runGreedyCheckResult(objectives, normalize)
 
         Assertions.assertEquals(JarTestHelper.BenchParameterized2.bench4, result[0].method)
         Assertions.assertEquals(JarTestHelper.BenchParameterized.bench1, result[1].method)
@@ -101,7 +52,7 @@ class GreedyTest {
     private fun oneObjectiveEqualWeightsCoverageChangeHistory(normalize: Boolean) {
         val objectives: List<Objective> = listOf(changeHistoryObjective)
 
-        val result = runGreedyCheckResults(objectives, normalize)
+        val result = runGreedyCheckResult(objectives, normalize)
 
         Assertions.assertEquals(JarTestHelper.BenchParameterized2.bench4, result[0].method)
         Assertions.assertEquals(JarTestHelper.OtherBench.bench3, result[1].method)
@@ -113,7 +64,7 @@ class GreedyTest {
     fun threeObjectivesEqualWeights() {
         val objectives: List<Objective> = listOf(covObjective, covOverlapObjective, changeHistoryObjective)
 
-        val result = runGreedyCheckResults(objectives, true)
+        val result = runGreedyCheckResult(objectives, true)
 
         Assertions.assertEquals(JarTestHelper.BenchParameterized.bench1, result[0].method)
         Assertions.assertEquals(JarTestHelper.BenchParameterized2.bench4, result[1].method)
